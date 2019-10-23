3ce6fa24d389014488b8d365cd8d25600d2b1416
yue <522656243@qq.com>
2019-10-23 14:07:27 +0800

    添加android Toast测试方法

--- android/src/main/java/com/nong/baidutrace/RNBaiduTraceModule.java
+++ android/src/main/java/com/nong/baidutrace/RNBaiduTraceModule.java
@@ -1,22 +1,43 @@
 
 package com.nong.baidutrace;
 
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import java.util.HashMap;
+import java.util.Map;
 
-public class RNBaiduTraceModule extends ReactContextBaseJavaModule {
-
+public class RNBaidutraceModule extends ReactContextBaseJavaModule {
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
   private final ReactApplicationContext reactContext;
 
-  public RNBaiduTraceModule(ReactApplicationContext reactContext) {
+  public RNBaidutraceModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
   }
 
   @Override
   public String getName() {
-    return "RNBaiduTrace";
+    return "RNBaidutrace";
   }
+
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+
+
+
 }
--- android/src/main/java/com/nong/baidutrace/RNBaiduTracePackage.java
+++ android/src/main/java/com/nong/baidutrace/RNBaiduTracePackage.java
@@ -10,10 +10,11 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNBaiduTracePackage implements ReactPackage {
+
+public class RNBaidutracePackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNBaiduTraceModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNBaidutraceModule(reactContext));
     }
 
     // Deprecated from RN 0.47
