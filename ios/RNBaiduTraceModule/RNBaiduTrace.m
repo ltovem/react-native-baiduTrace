8b1360e88fe86f916d0171a909e89ad2df05129c
ltove-work <t@ltove.com>
2019-10-24 11:52:40 +0800

    a

--- ios/RNBaiduTraceModule/RNBaiduTrace.m
+++ ios/RNBaiduTraceModule/RNBaiduTrace.m
@@ -3,8 +3,8 @@
 
 #import <BaiduTraceSDK/BaiduTraceSDK.h>
 
-@interface RNBaiduTrace ()
-
+@interface RNBaiduTrace ()<BTKTraceDelegate>
+@property (nonatomic,copy)NSString *entryName;
 @end
 
 @implementation RNBaiduTrace
@@ -17,18 +17,141 @@
 RCT_EXPORT_MODULE()
 
 /**
-* 初始化百度鹰眼轨迹服务
-* @param serviceId  // 轨迹服务ID string
-* @param entityName  // 设备标识 string
-* @param isNeedObjectStorage   // 是否需要对象存储服务，默认为：false，关闭对象存储服务。
-*                              注：鹰眼 Android SDK v3.0以上版本支持随轨迹上传图像等对象数据，
-*                              若需使用此功能，该参数需设为 true，且需导入bos-android-sdk-1.0.2.jar。
+ 初始化百度鹰眼轨迹服务
+ @param entityName  // 设备标识 string
+ @param ak ak
+ @param mcode mcode
+ @param serviceID 轨迹服务的ID
+ @param keepAlive 是否保活
 */
 RCT_EXPORT_METHOD(initBaiduTrace:(NSUInteger )serviceId ak:(NSString *)ak entityName:(NSString *)entityName bundleId:(NSString *)bundleId keepAlive:(BOOL)keepAlive){
-    
+    self.entryName = entityName;
     BTKServiceOption *sop = [[BTKServiceOption alloc] initWithAK:ak mcode:bundleId serviceID:serviceId keepAlive:keepAlive];
     [[BTKAction sharedInstance] initInfo:sop];
 }
+/**
+*   开启轨迹服务
+*/
+RCT_EXPORT_METHOD(startBaiduTrace){
+    // 设置开启轨迹服务时的服务选项，指定本次服务以“entityA”的名义开启
+    BTKStartServiceOption *op = [[BTKStartServiceOption alloc] initWithEntityName:self.entryName];
+    // 开启服务
+    [[BTKAction sharedInstance] startService:op delegate:self];
+}
+/**
+停止轨迹服务
 
+@param delegate 操作结果的回调对象
+*/
+RCT_EXPORT_METHOD(stopBaiduTrace){
+    [[BTKAction sharedInstance] stopService:self];
+}
+/**
+开始采集
+
+@param delegate 操作结果的回调对象
+*/
+RCT_EXPORT_METHOD(startBaiduTraceGather){
+    [[BTKAction sharedInstance] startGather:self];
+}
+/**
+停止采集
+
+@param delegate 操作结果的回调对象
+*/
+RCT_EXPORT_METHOD(stopBaiduTraceGather){
+    [[BTKAction sharedInstance] stopGather:self];
+}
+
+
+#pragma mark - BTKTraceDelegate
+/**
+ 开启轨迹服务的回调方法
+
+ @param error 开启轨迹服务的结果
+ */
+-(void)onStartService:(BTKServiceErrorCode) error{
+    NSLog(@"开启服务结果为 %lu",(unsigned long)error);
+}
+/**
+ 停止轨迹服务的回调方法
+
+ @param error 停止轨迹服务的结果
+ */
+-(void)onStopService:(BTKServiceErrorCode) error{
+    
+}
+
+/**
+ 开始采集的回调方法
+
+ @param error 开始采集的操作结果
+ */
+-(void)onStartGather:(BTKGatherErrorCode) error{
+    
+}
+/**
+ 停止采集的回调方法
+
+ @param error 停止采集的操作结果
+ */
+-(void)onStopGather:(BTKGatherErrorCode) error{
+    
+}
+
+/**
+ 收到推送消息的回调方法
+
+ @param message 推送消息的内容
+ */
+-(void)onGetPushMessage:(BTKPushMessage *)message{
+    
+}
+
+/**
+ 用户自定义信息的设置方法，SDK在每个采集周期会回调此方法，将此方法的返回值作为该定位周期内轨迹点的附加数据
+ 所有附加数据组成的字典，总的字节数不能超过1024字节。
+
+ @return 当前定位周期内轨迹点的附加数据，key代表自定义字段名称，value代表自定义字段的值。用法见DEMO
+ */
+//-(NSDictionary *)onGetCustomData{
+//
+//}
+
+/**
+ 用户自定义信息设置结果的回调方法
+
+ @param error 自定义信息的设置结果
+ */
+-(void)onGetCustomDataResult:(BTKGetCustomDataErrorCode) error{
+    
+}
+
+/**
+ 更改采集和打包上传周期的结果的回调方法
+
+ @param error 更改周期的结果
+ */
+-(void)onChangeGatherAndPackIntervals:(BTKChangeIntervalErrorCode) error{
+    
+}
+
+/**
+ 设置缓存占用的最大磁盘空间的结果的回调方法
+
+ @param error 设置的结果
+ */
+-(void)onSetCacheMaxSize:(BTKSetCacheMaxSizeErrorCode) error{
+    
+}
+
+/**
+请求后台定位权限的回调方法
+
+@param locationManager 定位控制器，开发者需要调用该实例的requestAlwaysAuthorization方法
+*/
+-(void)onRequestAlwaysLocationAuthorization:(CLLocationManager *) locationManager{
+    
+}
 @end
   
